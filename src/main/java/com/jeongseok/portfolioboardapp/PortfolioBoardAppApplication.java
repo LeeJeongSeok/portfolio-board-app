813345b9994dd8897b4d50a260848482fa27bcf9
Lee <ljs0429777@gmail.com>
2023-09-12 16:53:27 +0900

    Initial commit

---
+++ src/main/java/com/jeongseok/portfolioboardapp/PortfolioBoardAppApplication.java
@@ -0,0 +1,13 @@
+package com.jeongseok.portfolioboardapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioBoardAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioBoardAppApplication.class, args);
+	}
+
+}
