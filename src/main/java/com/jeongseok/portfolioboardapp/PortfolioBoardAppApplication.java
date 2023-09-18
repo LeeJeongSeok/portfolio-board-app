ef4604a79eddf7d6ddbd536135ac0eabe71c105e
Lee <ljs0429777@gmail.com>
2023-09-18 16:57:47 +0900

    update : http hidden method 설정 변경

--- src/main/java/com/jeongseok/portfolioboardapp/PortfolioBoardAppApplication.java
+++ src/main/java/com/jeongseok/portfolioboardapp/PortfolioBoardAppApplication.java
@@ -2,9 +2,7 @@ package com.jeongseok.portfolioboardapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -12,11 +10,6 @@ public class PortfolioBoardAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioBoardAppApplication.class, args);
-	}
-
-	@Bean
-	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
-		return new HiddenHttpMethodFilter();
 	}
 
 }
